6c60086ebd9f557eec85d1f2a606bbd5eedd2844
BrandeYarborough <brandeyarborough@gmail.com>
2023-01-30 16:26:24 -0500

    completes like button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [likes, setLikes] = useState(0);
+  const addLike = () => {
+    setLikes(likes + 1);
+  };
+  const changeLike = () => {
+    if (likes === 1) {
+      return `${likes} like`;
+    }else {
+      return `${likes} likes`;
+    }
+  };
+  
+  
+
+return (
+<div className="App">
+  <button onClick={addLike}>{changeLike()}</button>
+</div>
+);
 }
 
 export default App;
